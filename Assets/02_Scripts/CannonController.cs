d2022e567192c823816f50f35cca0fa2f8919723
LeeJaeHyun <myleje@gmail.com>
2024-09-28 14:44:08 +0900

    .

--- Assets/02_Scripts/CannonController.cs
+++ Assets/02_Scripts/CannonController.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
+using Photon.Pun;
 
 public class CannonController : MonoBehaviour
 {
     private float r;
     [SerializeField] private float speed = 200.0f;
+
+    void Start()
+    {
+        this.enabled = transform.root.GetComponent<PhotonView>().IsMine;
+    }
 
     void Update()
     {
