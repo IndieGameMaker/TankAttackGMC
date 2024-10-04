55fd055466758abe957f04f0194d9f19cf3c7eba
LeeJaeHyun <myleje@gmail.com>
2024-10-04 11:44:20 +0900

    .

--- Assets/02_Scripts/GameManager.cs
+++ Assets/02_Scripts/GameManager.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
-    void Start()
+    IEnumerator Start()
     {
+        yield return new WaitForSeconds(0.5f);
 
+        CreateTank();
     }
 
     private void CreateTank()
     {
+        // 탱크 생성할 좌표 
+        Vector3 pos = new Vector3(Random.Range(-20.0f, 20.0f),
+                                  10.0f,
+                                  Random.Range(-20.0f, 20.0f));
 
+        // 네트워크 탱크 생성
+        PhotonNetwork.Instantiate("Tank", new Vector3(0, 10.0f, 0), Quaternion.identity, 0);
     }
 }
