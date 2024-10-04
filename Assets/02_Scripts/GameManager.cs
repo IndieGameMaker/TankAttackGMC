f4dece95dc1f333545dfafa055d889b597e047d9
LeeJaeHyun <myleje@gmail.com>
2024-10-04 11:40:24 +0900

    .

---
+++ Assets/02_Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    private void CreateTank()
+    {
+
+    }
+}
